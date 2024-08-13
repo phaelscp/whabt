3c8f5c4d1c1631fc7bbb96f4d0abdc86fec1137c
phaelscp <phaelunder@gmail.com>
2024-08-13 14:46:18 -0300

    first commit

---
+++ frontend/whaticketplus/precache-manifest.608fca00085f3698ed8bee63a4bccfd7.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfefa2797c0204bc7e67cf5abf5ee241",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a441bc54985246a96b9",
+    "url": "/static/css/2.a02ef358.chunk.css"
+  },
+  {
+    "revision": "c4acfee5606634597040",
+    "url": "/static/css/main.eff2a340.chunk.css"
+  },
+  {
+    "revision": "7a441bc54985246a96b9",
+    "url": "/static/js/2.90343950.chunk.js"
+  },
+  {
+    "revision": "d6e716ca8a49ab5627cc3f25d7f85e01",
+    "url": "/static/js/2.90343950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e639c06978235e79cd52",
+    "url": "/static/js/3.1cbb1479.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/static/js/3.1cbb1479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4acfee5606634597040",
+    "url": "/static/js/main.8188aac0.chunk.js"
+  },
+  {
+    "revision": "aa6857ab5cfd7f80ffb6",
+    "url": "/static/js/runtime-main.94aff927.js"
+  },
+  {
+    "revision": "decf0fdf8bf2b85296b7ea5d04fdf0d3",
+    "url": "/static/media/Dashboard1.decf0fdf.jpg"
+  },
+  {
+    "revision": "15a4c6c9147b04cea0ec3485ff246c82",
+    "url": "/static/media/Dashboard2.15a4c6c9.jpeg"
+  },
+  {
+    "revision": "9be9d0771c39e93df18a8facd1b09f9b",
+    "url": "/static/media/Dashboard3.9be9d077.jpeg"
+  },
+  {
+    "revision": "da5f0f49b8dffc6b03a704cd3753bc1a",
+    "url": "/static/media/Login1.da5f0f49.jpeg"
+  },
+  {
+    "revision": "b3f1a51e1c42dd74e758b65367ed56b2",
+    "url": "/static/media/Login2.b3f1a51e.jpeg"
+  },
+  {
+    "revision": "bb026baec0a8c658c15dd2b8f2e947f3",
+    "url": "/static/media/Login3.bb026bae.png"
+  },
+  {
+    "revision": "275115462e667571d2bfed170deff5bf",
+    "url": "/static/media/SignUp1.27511546.jpeg"
+  },
+  {
+    "revision": "9c8d898e7f8923345dad4c9fadd6d85e",
+    "url": "/static/media/SignUp2.9c8d898e.jpeg"
+  },
+  {
+    "revision": "619c3a656f5f63bd64ff7d5655312bb8",
+    "url": "/static/media/TicketsManagerTabs1.619c3a65.jpg"
+  },
+  {
+    "revision": "362fffa6d87feb7e5bda2f39b819f075",
+    "url": "/static/media/TicketsManagerTabs2.362fffa6.jpg"
+  },
+  {
+    "revision": "29b59e69a458f15be9b3efa8cc43e6d5",
+    "url": "/static/media/TicketsManagerTabs3.29b59e69.png"
+  },
+  {
+    "revision": "f79143db55ef64589a613a7ab2b27cd1",
+    "url": "/static/media/bg-login.f79143db.png"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "c75e27c9a280a044f0b39de02ff22bd9",
+    "url": "/static/media/email.c75e27c9.png"
+  },
+  {
+    "revision": "325c3d0cfb7292322f9a7e1ecf1be0e7",
+    "url": "/static/media/fundo.325c3d0c.mp4"
+  },
+  {
+    "revision": "0b6b3c8d2c74fc2e0be8f5d940ec1e14",
+    "url": "/static/media/instagram.0b6b3c8d.png"
+  },
+  {
+    "revision": "b801b20b421f5c72fc24dbb1e9db3ca6",
+    "url": "/static/media/logo.b801b20b.png"
+  },
+  {
+    "revision": "5c788b8d54d3ebf021114528116dfd77",
+    "url": "/static/media/n8n.5c788b8d.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "786a3db7f3dbf4bbc27efd28d726e6fe",
+    "url": "/static/media/success.786a3db7.gif"
+  },
+  {
+    "revision": "8244f0423cb09d847ac99e7546cb9220",
+    "url": "/static/media/wa-background-dark.8244f042.png"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/static/media/wa-background.adbccee0.png"
+  },
+  {
+    "revision": "e5374d73ff43538fdaa16a3f1a980f4b",
+    "url": "/static/media/webhook.e5374d73.png"
+  },
+  {
+    "revision": "ae8a9a2fbeb676e0a58add382ddea93a",
+    "url": "/static/media/whatsapp.ae8a9a2f.png"
+  }
+]);
